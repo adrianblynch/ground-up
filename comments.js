8f0af3df6645a18e00995d06868229b12a3faead
Adrian Lynch <adrian.lynch@concrete.cc>
2014-11-05 22:51:31 +0000

    Initial commit

---
+++ comments.js
@@ -0,0 +1,26 @@
+/*
+
+Meteor
+
+What is it?
+
+A real time node application that runs on the client and server.
+
+You'll need to...
+
+- Download and install Meteor by running `curl https://install.meteor.com/ | sh`
+- Create a new application with `meteor create myApp`
+- Go into the newly created app directory and run `meteor`
+- Browse to localhost:3000 to view the new app
+
+Commands
+
+To see the data in Mongo `meteor mongo`
+
+To deploy `meteor deploy ground-up.meteor.com`
+
+To remove deployment `meteor deploy --delete ground-up.meteor.com`
+
+To clear Mongo collection ``
+
+*/
---
+++ ground-up.js
@@ -0,0 +1,70 @@
+// 1. Setup for the incrementor, a helper and events
+
+ if (Meteor.isClient) {
+
+	// Set default at the start
+	Session.setDefault("counter", 0);
+
+	// Provide the template with a way to get the current counter
+	Template.hello.helpers({
+		counter: function () {
+			return Session.get("counter");
+		}
+	});
+
+	// Map a button click to some behaviour
+	Template.hello.events({
+		'click button': function () {
+			Session.set("counter", Session.get("counter") + 1);
+		}
+	});
+
+}
+
+// 2. Loop over a collection
+
+/*if (Meteor.isClient) {
+
+	Template.body.helpers({
+		things: [
+			{text: "Thing 1"},
+			{text: "Thing 2"},
+			{text: "Thing 3"}
+		]
+	});
+
+}*/
+
+// 3a. Data in a database
+
+// Creates a new `things` collection available to both client and server (not on the server till the first insert)
+/*Things = new Mongo.Collection("things");
+
+if (Meteor.isClient) {
+
+	Template.body.helpers({
+		things: function() {
+			return Things.find();
+		}
+	});
+
+	// 3b. Nothing is in this collection until we run the following in the Mongo shell:
+	// db.tasks.insert({text: "Thing from the DB 1"});
+	// db.tasks.insert({text: "Thing from the DB 2"});
+
+	// 4. Event mapping to behaviour
+	// Template.body.events({
+	// 	"submit #newThing": function(event) {
+
+	// 		var newThing = event.target.newThing.value;
+
+	// 		Things.insert({text: newThing});
+
+	// 		event.target.newThing.value = "";
+
+	// 		return false;
+
+	// 	}
+	// });
+
+}*/
